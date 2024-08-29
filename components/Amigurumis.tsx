66da7c03a9821f213ddc7fb9bc94fa0966cf48b9
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2024-08-28 21:08:46 -0300

    After insert more images

--- components/Amigurumis.tsx
+++ components/Amigurumis.tsx
@@ -2,9 +2,36 @@ import Image from "next/image";
 
 const fotos = [
   { id: 1, title: "Mr. Sonicz", href: "/zozod.JPG" },
-  { id: 2, title: "Picaschultz", href: "/feioso.JPG" },
-  { id: 3, title: "Johnny Fox", href: "/ursinho.JPG" },
-  { id: 4, title: "Harry Bacon", href: "/porquinho.JPG" },
+  { id: 2, title: "Harry Bacon", href: "/porquinho.JPG" },
+  { id: 3, title: "Johnny Fox", href: "/Raposinho.png" },
+
+  { id: 4, title: "Picaschultz", href: "/orelhudo.png" },
+  { id: 5, title: "O Coisinha", href: "/coisinha.png" },
+  { id: 6, title: "Dinossaulo", href: "/dinossaulo.png " },
+
+  { id: 7, title: "Os Coelhinhos", href: "/coelhinhos.png" },
+  { id: 8, title: "Chase", href: "/caninoazul.JPG " },
+  { id: 9, title: "Rubble", href: "/caninoamarelo.JPG " },
+
+  { id: 10, title: "Skye", href: "/caninorosa.JPG " },
+  { id: 11, title: "Marshall", href: "/caninobranco.JPG " },
+  { id: 12, title: "Zuma", href: "/caninomarrom.JPG " },
+
+  { id: 13, title: "A turminha", href: "/turminha.png" },
+  { id: 14, title: "A Galerinha", href: "/galerinha.JPG " },
+  { id: 15, title: "Patrulha Canina", href: "/patrulha.JPG " },
+
+  { id: 16, title: "Rocky", href: "/caninocinza.JPG " },
+  { id: 17, title: "Zebrinha", href: "/zebrinho2.JPG " },
+  { id: 18, title: "Santa Inês", href: "/Santa.JPG " },
+
+  { id: 19, title: "Blau-Blau", href: "/ursinhoverde.JPG " },
+  { id: 20, title: "Girafinha de Montar", href: "/girafinha2.JPG " },
+  { id: 21, title: "Emojis", href: "/chaveirinhos.JPG " },
+
+  { id: 22, title: "Elefantinho", href: "/elefantinho.JPG " },
+  { id: 23, title: "Oi Girafinha", href: "/girafinha1.JPG " },
+  { id: 24, title: "Oi Girafinho", href: "/girafinha2montada.JPG " },
 ];
 
 const Amigurumis = () => {
@@ -17,8 +44,8 @@ const Amigurumis = () => {
               <Image
                 src={foto.href}
                 alt={foto.title}
-                width={300}
-                height={300}
+                width={250}
+                height={250}
                 className="rounded-sm shadow-md shadow-violet-300"
               />
               <p className="text-violet-500 text-center text-lg">
