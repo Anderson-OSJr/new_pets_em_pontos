64c11bf26343e87ffe7152f648455a67aed8b9a9
ANDERSON DE OLIVEIRA E SILVA JUNIOR <87553205+Anderson-OSJr@users.noreply.github.com>
2024-08-31 18:34:09 -0300

    After bottom Bar design corrections requestes by the client

--- components/BottomBar.tsx
+++ components/BottomBar.tsx
@@ -6,7 +6,7 @@ const BottomBar = () => {
     <>
       <div
         className="container rounded-md flex flex-col items-center justify-between mt-3
-                   bg-purple-200 h-[140px] w-screen   
+                   bg-purple-200 h-[160px] w-screen   
                       sm:w-[640px]                  
                       md:w-[768px]
                       lg:w-[1024px] 
@@ -16,20 +16,16 @@ const BottomBar = () => {
         <p className="font-serif italic text-violet-800 text-opacity-70 mt-6 text-center">
           Momentos de felicidade, por meio de linhas e agulhas.
         </p>
-        <p className="font-serif italic text-violet-800 text-opacity-70 mb-2 text-center">
+        <p className="font-serif italic text-violet-800 text-opacity-70 mb-1 text-center">
           Com muito amor...
         </p>
-        <Image
-          src="/by_silvinha_barreto.png"
-          alt="Logo"
-          width={180}
-          height={100}
-          className="mb-3"
-        />
+        <p className="font-serif italic text-violet-800 text-opacity-70 mb-2 text-center">
+          Sílvia Barreto
+        </p>
 
         <p
           className="container text-violet-900 text-xs                  
-                    text-right mb-1 pr-1"
+                    text-right mb-1 pr-1 pb-2"
         >
           P61 Web Development
         </p>
